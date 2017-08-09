7c2c7e93b12e3116c0ea8b2192b942281cad6391
Chyld Medford <chyld.medford@gmail.com>
2017-08-09 20:36:32 +0000

    drop down

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,11 @@
 class TasksController < ApplicationController
     def index
         @task = Task.new
-        @tasks = Task.all
+        @tasks = Task.order(id: :desc)
     end
 
     def create
-        t = Task.new(params[:task].permit(:title))
+        t = Task.new(params[:task].permit(:title, :due, :priority, :is_complete, :category))
         t.save
         redirect_to tasks_url
     end
